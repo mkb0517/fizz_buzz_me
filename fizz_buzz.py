fd6f1d47f2093701026b9977a58dfda58d878699
mkb0517 <brownmk@hawaii.edi>
2015-11-21 10:20:06 -1000

    changed to one return and optimized the modulus checks a bit

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
+import sys
+
 def fizz_buzz(number):
     """Return a string representing the number, or 'fizz'/'buzz'/'fizzbuzz'"""
-    if number%3==0 and number%5==0:
-        return "fizzbuzz"
-    elif number%3==0:
-        return "fizz"
+    if number%3==0:
+        result = "fizz"
+        if number%5==0:
+            result = "fizzbuzz"
     elif number%5==0:
-        return "buzz"
+        result = "buzz"
     else:
-        return str(number) 
-        
+        result = str(number) 
+    return result    
 
 def main():
     """Print sample output of fizz_buzz function"""
     NUMBER = 20
     print("Sample fizz_buzz output for 1 to {0}:".format(NUMBER))
-    for i in range(1, NUMBER):
+    for i in range(1, NUMBER+1):
         print(fizz_buzz(i))
 
 
